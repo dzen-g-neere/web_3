90614f3bd5b746485a78aa28e36ca783315508da
zengineer <dmitrijzalevskij007@gmail.com>
2021-12-18 13:40:04 +0300

    clearDB() Bugfix

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -1,31 +1,16 @@
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+
 import javax.annotation.Resource;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import java.util.List;
 
 public class DatabaseManager {
     private static final String PERSISTENCE_UNIT_NAME = "hibernate";
+    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
     private EntityManager entityManager;
-    private EntityManagerFactory entityManagerFactory;
     @Resource
     private UserTransaction userTransaction;
-
-    @PostConstruct
-    public void init() {
-        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @PreDestroy
-    public void destroy() {
-        entityManager.close();
-        entityManagerFactory.close();
-    }
 
     public List<Point> loadEntries() {
         return entityManager.createQuery("SELECT e FROM Point e").getResultList();
@@ -38,8 +23,6 @@ public class DatabaseManager {
     }
 
     public synchronized void clearDB() throws Exception {
-        userTransaction.begin();
         entityManager.createQuery("DELETE FROM Point").executeUpdate();
-        userTransaction.commit();
     }
 }
